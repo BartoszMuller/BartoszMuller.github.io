24ef2c8c2f8071e7e81ee39166d02ccd14ca23d6
BartoszMullerOtree <bmuller@otree.tech>
2025-03-13 13:19:14 +0100

    Some fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,15 @@
+import fadeInAnimation from "./fade_in_animation.js";
 import onFirstTimeSection from "./on_first_time_section.js";
 import onFirstTimeView from "./on_first_time_view.js";
 import currentSection from "./on_window_currency.js";
 import pageMainEvents from "./page_main_events.js";
+import scrollAnimate from "./scroll_animate.js";
 import swiperInit from "./swiper_init.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   pageMainEvents();
+  scrollAnimate();
+  fadeInAnimation();
   currentSection();
   onFirstTimeView();
   swiperInit();
--- assets/scripts/page_main_events.js
+++ assets/scripts/page_main_events.js
@@ -6,53 +6,57 @@ export default function pageMainEvents() {
     return window.innerWidth <= 860;
   };
 
+  
   const pageContainer = document.getElementById("main-container");
   const header = document.getElementById("main-header");
-
+  
   if (header) {
     const disableScroll = () => {
       document.removeEventListener("scroll", headerStickyHandler);
-
+      
       pageScrollY = window.scrollY;
       pageContainer.classList.add("scrollLock");
       pageContainer.style.height = "calc( 100svh + " + pageScrollY + "px)";
-
+      
       isScrollDisable = true;
     };
-
+    
     const enableScroll = () => {
       pageContainer.classList.remove("scrollLock");
       pageContainer.style.height = "unset";
       document.documentElement.style.scrollBehavior = "auto";
       window.scrollTo(0, pageScrollY);
       document.documentElement.style.scrollBehavior = "smooth";
-
+      
       document.addEventListener("scroll", headerStickyHandler);
-
+      
       isScrollDisable = false;
     };
-
+    
     // MENU & HEADER STICKY
-
+    
     const menuButton = document.getElementById("menu-button");
     const menuNav = document.getElementById("menu-nav");
-
+    
     menuButton?.addEventListener("click", (event) => {
       event.preventDefault();
-
+      
       menuButton?.classList.toggle("isClicked");
       menuNav?.classList.toggle("isOpen");
       setTimeout(() => window.scrollTo(0, window.scrollY), 1);
-
+      
       isScrollDisable ? enableScroll() : disableScroll();
     });
-
+    
+    const menuClick = () => {
+      menuButton.dispatchEvent(new Event('click'));
+    }
     let prevScrollY = 0;
     let showHeaderTimeout;
     const showHeader = () => {
       header.classList.remove("isHidden");
     };
-
+    
     const headerStickyHandler = () => {
       const scrollY = window.scrollY;
 
@@ -87,7 +91,11 @@ export default function pageMainEvents() {
             const elementLink = document.querySelector(menuLink.hash);
 
             elementLink.style.scrollMarginTop = headerHeight + "px";
-            elementLink?.scrollIntoView({ block: "start" });
+            isScrollDisable && menuClick();
+            setTimeout(() => {
+              elementLink?.scrollIntoView({ block: "start" });
+
+            }, 100);
           });
         }
       });
